68dfe7626a8214279d995fa5ebe9bab1918b6bc1
laurent ongaro <laurent@gameamea.com>
2019-02-07 15:58:57 +0100

    unity 2018.3 compatibility (WIP)

--- UnityDarkSkin/Program.cs
+++ UnityDarkSkin/Program.cs
@@ -1,293 +1,326 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace UnityDarkSkin
-{
-    class Program
-    {
-        static SkinType Skin;
-        static Arch SystemType;
+namespace UnityDarkSkin {
+  internal static class Program {
+    private const bool askForSystemVersion = true;
+    private const bool askForUnityVersion = true;
 
-        static string FilePath;
-        static string FileName = "Unity.exe";
-        static int BytePosition;
-        static string[] Signatures;
-        static uint[] InjectionCodes;
+    private static SkinType Skin;
+    private static Arch SystemType;
+    private static Version UnityVersion;
 
-        static void Main(string[] args)
-        {
-            Init();
-            Setup();
-            Start();
-        }
+    private static string FilePath;
+    private const string FileName = "Unity.exe";
+    private const string FilePathBackup = FileName + ".BAK";
 
-        static void Init()
-        {
-            Console.Title = "Unity Dark Skin";
-            Console.WriteLine("Choose version:");
-            Console.WriteLine("Unity.exe (32 bit): type '1'");
-            Console.WriteLine("Unity.exe (64 bit): type '2'");
-            Console.Write("Your answer: ");
+    private static int BytePosition;
+    private static string[] Signatures;
+    private static string[] InjectionCodes;
 
-            ConsoleKeyInfo key = Console.ReadKey();
-            switch (key.KeyChar)
-            {
-                case '1':
-                    SystemType = Arch.x86;
-                    break;
-                case '2':
-                default:
-                    SystemType = Arch.x64;
-                    break;
-            }
-        }
-
-        static void Setup()
-        {
-            switch (SystemType)
-            {
-                case Arch.x86:
-                    Signatures = new string[] {
-                        "750433C05EC38B065EC3CCCCCCCCCCCCCCCC",
-                        "740433C05EC38B065EC3CCCCCCCCCCCCCCCC",
-                    };
-
-                    InjectionCodes = new uint[] {
-                        3224568949U,
-                        3224568948U,
-                    };
-                    break;
-                case Arch.x64:
-                    Signatures = new string[] {
-                        "750833C04883C4205BC38B034883C4205BC3",
-                        "740833C04883C4205BC38B034883C4205BC3",
-                    };
-
-                    InjectionCodes = new uint[] {
-                        3224569973U,
-                        3224569972U,
-                    };
-                    break;
-            }
-        }
-
-        static void Start()
-        {
-            string Directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            FilePath = Directory + @"\" + FileName;
-            Console.WriteLine("\nSearch path: " + FilePath);
-
-            if (File.Exists(FilePath))
-            {
-                Console.WriteLine("--------");
-                Console.WriteLine("Attention: make backup of your Unity.exe file!");
-                Console.WriteLine("Press any key to change skin...");
-                Console.ReadKey();
-
-                BytePosition = GetBytePosition();
-
-                try
-                {
-                    if (BytePosition != 0)
-                    {
-                        Console.WriteLine("--------");
-
-                        GetSkinType(BytePosition);
-                        Console.WriteLine("Current skin: " + Skin.ToString());
-
-                        Console.WriteLine("Please wait...");
-                        ToggleSkinType();
-
-                        GetSkinType(BytePosition);
-                        Console.WriteLine("Current skin: " + Skin.ToString());
-                    }
-                    else
-                    {
-                        Console.WriteLine("--------");
-                        Console.WriteLine("Signature is not found. Choose another file.");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("--------");
-                    Console.WriteLine("Error is ocurred...");
-                    Console.WriteLine("Run application as an Adminisrator");
-                }
-
-                Console.WriteLine("--------");
-                Console.WriteLine("Done!");
-            }
-            else
-            {
-                Console.WriteLine(FileName + " not found.\nPlease copy this application to folder with " + FileName);
-            }
-
-            Console.ReadKey();
-        }
-
-        static void ChooseFolder()
-        {
-            Console.WriteLine("\n--------");
-            Console.WriteLine("Is Unity.exe with this .exe?");
-            Console.WriteLine("Unity.exe is here!: type 'y'");
-            Console.WriteLine("I'll choose my Unity.exe folder: type 'n'");
-            Console.Write("Your answer: ");
-
-            ConsoleKeyInfo key = Console.ReadKey();
-            string UnityFolder = "";
-            switch (key.KeyChar)
-            {
-                case 'n':
-                    Console.Write("\nEnter your file path: ");
-                    UnityFolder = Console.ReadLine();
-                    break;
-                case 'y':
-                default:
-                    UnityFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-                    break;
-            }
-            
-            FilePath = UnityFolder + @"\" + FileName;
-        }
-
-        static void ToggleSkinType()
-        {
-            switch (Skin)
-            {
-                case SkinType.Light:
-                    SwitchSkin(1);
-                    break;
-                case SkinType.Dark:
-                    SwitchSkin(0);
-                    break;
-            }
-        }
-
-        static void SwitchSkin(int t)
-        {
-            switch (t)
-            {
-                case 0:
-                    using (BinaryWriter binaryWriter = new BinaryWriter((Stream)File.OpenWrite(FilePath)))
-                    {
-                        binaryWriter.BaseStream.Position = (long)BytePosition;
-                        binaryWriter.Write(InjectionCodes[0]);
-                        binaryWriter.Flush();
-                        binaryWriter.Close();
-                    }
-                    Skin = SkinType.Light;
-                    break;
-                case 1:
-                    using (BinaryWriter binaryWriter = new BinaryWriter((Stream)File.OpenWrite(FilePath)))
-                    {
-                        binaryWriter.BaseStream.Position = (long)BytePosition;
-                        binaryWriter.Write(InjectionCodes[1]);
-                        binaryWriter.Flush();
-                        binaryWriter.Close();
-                    }
-                    Skin = SkinType.Dark;
-                    break;
-            }
-        }
-
-        static void GetSkinType(int offset)
-        {
-            using (BinaryReader binaryReader = new BinaryReader((Stream)File.OpenRead(FilePath)))
-            {
-                binaryReader.BaseStream.Position = (long)offset;
-                switch (binaryReader.ReadByte().ToString("X2"))
-                {
-                    case "75":
-                        Skin = SkinType.Light;
-                        break;
-                    case "74":
-                        Skin = SkinType.Dark;
-                        break;
-                }
-                binaryReader.Close();
-            }
-        }
-
-        // Returns offset of signature
-        static int GetBytePosition()
-        {
-            int position = 0;
-            using (BinaryReader binaryReader = new BinaryReader((Stream)File.OpenRead(FilePath)))
-            {
-                foreach (string Signature in Signatures)
-                {
-                    byte[] SignatureBytes = StringToByteArray(Signature);
-                    binaryReader.BaseStream.Position = 0L;
-                    byte[] FileBytes = new byte[binaryReader.BaseStream.Length];
-
-                    binaryReader.Read(FileBytes, 0, FileBytes.Length);
-                    int pos = FindSignature(FileBytes, SignatureBytes, 0);
-                    if (pos != -1)
-                    {
-                        position = pos;
-                        break;
-                    }
-                }
-                binaryReader.Close();
-            }
-            return position;
-        }
-
-        // Searches in an executable file
-        static int FindSignature(byte[] bytes, byte[] search, int offset = 0)
-        {
-            int num = -1;
-            if (bytes.Length > 0 && search.Length > 0 && (offset <= bytes.Length - search.Length && bytes.Length >= search.Length))
-            {
-                for (int i = offset; i <= bytes.Length - search.Length; ++i)
-                {
-                    if (bytes[i] == search[0])
-                    {
-                        if (bytes.Length > 1)
-                        {
-                            bool flag = true;
-                            for (int j = 1; j < search.Length; ++j)
-                            {
-                                if (bytes[i + j] != search[j])
-                                {
-                                    flag = false;
-                                    break;
-                                }
-                            }
-                            if (flag)
-                            {
-                                num = i;
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            num = i;
-                            break;
-                        }
-                    }
-                }
-            }
-            return num;
-        }
-
-        // Hex string to array of bytes
-        static byte[] StringToByteArray(string hex)
-        {
-            return Enumerable.Range(0, hex.Length).Where(x => x % 2 == 0).Select((x) => Convert.ToByte(hex.Substring(x, 2), 16)).ToArray();
-        }
-
-        enum Arch
-        {
-            x86,
-            x64,
-        }
-
-        enum SkinType
-        {
-            Dark,
-            Light,
-        }
+    private static void Main (string[] args) {
+      Init();
+      var path = "";
+      if (args.Length > 0) path = args[0];
+      ChooseFolder(path);
+      Setup();
+      Start();
     }
+
+    private static void Init () {
+      Console.Title = "Unity Dark Skin";
+      SystemType = Arch.x64;
+      if (askForSystemVersion) {
+        Console.WriteLine("Choose system version:");
+        Console.WriteLine("Unity.exe (64 bit): type '1'");
+        Console.WriteLine("Unity.exe (32 bit): type '2'");
+        Console.Write("\nYour answer: ");
+
+        ConsoleKeyInfo key = Console.ReadKey();
+        switch (key.KeyChar) {
+          case '2':
+            SystemType = Arch.x86;
+            break;
+          default:
+            SystemType = Arch.x64;
+            break;
+        }
+      }
+
+      UnityVersion = Version.higherThan2018_2;
+      if (askForUnityVersion) {
+        Console.WriteLine("\n\nChoose unity version:");
+        Console.WriteLine("2018.3 or more: type '1'");
+        Console.WriteLine("2018.2 or less: type '2'");
+        Console.Write("\nYour answer: ");
+
+        ConsoleKeyInfo key = Console.ReadKey();
+        switch (key.KeyChar) {
+          case '2':
+            UnityVersion = Version.lowerThan2018_2;
+            break;
+          default:
+            UnityVersion = Version.higherThan2018_2;
+            break;
+        }
+      }
+    }
+
+    private static void Setup () {
+      if (UnityVersion == Version.higherThan2018_2) {
+        if (SystemType == Arch.x64) {
+          Signatures = new[] {
+              "84C0750833C04883C4305BC38B034883C4305BC3",
+              "84C0740833C04883C4305BC38B034883C4305BC3﻿"
+          };
+
+          InjectionCodes = new[] {
+              "84C074",
+              "84C075"
+          };
+        }
+        else if (SystemType == Arch.x86) {
+          // the following values are NOT THE GOOD ONE
+          Signatures = new[] {
+              "84C0750833C04883C4305BC38B034883C4305BC3",
+              "84C0740833C04883C4305BC38B034883C4305BC3﻿"
+          };
+
+          InjectionCodes = new[] {
+              "84C074",
+              "84C075"
+          };
+        }
+      }
+      else {
+        if (SystemType == Arch.x86) {
+          // the following values are NOT THE GOOD ONE
+          Signatures = new[] {
+              "84C0750833C04883C4305BC38B034883C4305BC3",
+              "84C0740833C04883C4305BC38B034883C4305BC3﻿"
+          };
+
+          InjectionCodes = new[] {
+              "84C074",
+              "84C075"
+          };
+        }
+        else if (SystemType == Arch.x64) {
+          // the following values are NOT THE GOOD ONE
+          Signatures = new[] {
+              "84C0750833C04883C4305BC38B034883C4305BC3",
+              "84C0740833C04883C4305BC38B034883C4305BC3﻿"
+          };
+
+          InjectionCodes = new[] {
+              "84C074",
+              "84C075"
+          };
+        }
+      }
+    }
+
+    private static void Start () {
+      var Directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+      FilePath = Directory + @"\" + FileName;
+      Console.WriteLine("\nSearch unityExe: " + FilePath);
+
+      if (File.Exists(FilePath)) {
+        Console.WriteLine("--------");
+        Console.WriteLine("Press any key to change skin...");
+        Console.ReadKey();
+        if (!File.Exists(FilePathBackup)) {
+          Console.WriteLine("Unity.exe will be backed up as " + FilePathBackup);
+          //File.Delete(FilePathBackup);
+          File.Copy(FilePath, FilePathBackup);
+        }
+        else {
+          Console.WriteLine("Unity.exe backup already exists");
+        }
+
+        BytePosition = GetBytePosition();
+
+        try {
+          if (BytePosition != 0) {
+            Console.WriteLine("--------");
+
+            GetSkinType(BytePosition);
+            Console.WriteLine("Current skin: " + Skin);
+
+            Console.WriteLine("Please wait...");
+            ToggleSkinType();
+
+            GetSkinType(BytePosition);
+            Console.WriteLine("Current skin: " + Skin);
+          }
+          else {
+            Console.WriteLine("--------");
+            Console.WriteLine("Signature is not found. Choose another file.");
+          }
+        }
+        catch (Exception e) {
+          Console.WriteLine("--------");
+          Console.WriteLine("Error has occured:" + e.Message);
+          Console.WriteLine("Run application as an Administrator");
+        }
+
+        Console.WriteLine("--------");
+        Console.WriteLine("Done!");
+      }
+      else {
+        Console.WriteLine(FileName + " not found.\nPlease copy this application to folder with " + FileName);
+      }
+
+      Console.ReadKey();
+    }
+
+    private static void ChooseFolder (string unityExe = "") {
+      Console.WriteLine("\n--------");
+      Console.WriteLine("Is Unity.exe located in this file folder (y/n) ?");
+      Console.WriteLine("To specify the unity.exe folder type 'n'");
+      Console.Write("Your answer: ");
+
+      ConsoleKeyInfo key = Console.ReadKey();
+      string UnityFolder;
+      switch (key.KeyChar) {
+        case 'n':
+          Console.Write("\nEnter the unity.exe file path: ");
+          UnityFolder = Console.ReadLine();
+          break;
+        default:
+          UnityFolder = ("" == unityExe) ? Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) : Path.GetDirectoryName(unityExe);
+          break;
+      }
+
+      FilePath = UnityFolder + @"\" + FileName;
+    }
+
+    private static void ToggleSkinType () {
+      switch (Skin) {
+        case SkinType.Light:
+          SwitchSkin(1);
+          break;
+        case SkinType.Dark:
+          SwitchSkin(0);
+          break;
+      }
+    }
+
+    private static void SwitchSkin (int t) {
+      byte[] value;
+      switch (t) {
+        case 0:
+          using (BinaryWriter binaryWriter = new BinaryWriter(File.OpenWrite(FilePath))) {
+            binaryWriter.BaseStream.Position = BytePosition;
+            //value = int.Parse(InjectionCodes[0], System.Globalization.NumberStyles.HexNumber);
+            value = StringToByteArray(InjectionCodes[1]);
+            binaryWriter.Write(value);
+            binaryWriter.Flush();
+            binaryWriter.Close();
+          }
+
+          Skin = SkinType.Light;
+          break;
+        case 1:
+          using (BinaryWriter binaryWriter = new BinaryWriter(File.OpenWrite(FilePath))) {
+            binaryWriter.BaseStream.Position = BytePosition;
+            //value = int.Parse(InjectionCodes[1], System.Globalization.NumberStyles.HexNumber);
+            value = StringToByteArray(InjectionCodes[0]);
+            binaryWriter.Write(value);
+            binaryWriter.Flush();
+            binaryWriter.Close();
+          }
+
+          Skin = SkinType.Dark;
+          break;
+      }
+    }
+
+    private static void GetSkinType (int offset) {
+      using (BinaryReader binaryReader = new BinaryReader(File.OpenRead(FilePath))) {
+        binaryReader.BaseStream.Position = offset + 2;
+        var skinFlag = binaryReader.ReadByte().ToString("X2");
+        switch (skinFlag) {
+          case "75":
+            Skin = SkinType.Light;
+            break;
+          case "74":
+            Skin = SkinType.Dark;
+            break;
+        }
+
+        binaryReader.Close();
+      }
+    }
+
+    // Returns offset of signature
+    private static int GetBytePosition () {
+      var position = 0;
+      using (BinaryReader binaryReader = new BinaryReader(File.OpenRead(FilePath))) {
+        var FileBytes = new byte[binaryReader.BaseStream.Length];
+        foreach (var Signature in Signatures) {
+          var SignatureBytes = StringToByteArray(Signature);
+          binaryReader.BaseStream.Position = 0L;
+
+          binaryReader.Read(FileBytes, 0, FileBytes.Length);
+          var pos = FindSignature(FileBytes, SignatureBytes);
+          if (pos == -1) continue;
+          position = pos;
+          break;
+        }
+
+        binaryReader.Close();
+      }
+
+      return position;
+    }
+
+    // Searches in an executable file
+    private static int FindSignature (IReadOnlyList<byte> bytes, IReadOnlyList<byte> search, int offset = 0) {
+      var num = -1;
+      if (bytes.Count <= 0 || search.Count <= 0 || (offset > bytes.Count - search.Count || bytes.Count < search.Count)) return num;
+      for (var i = offset; i <= bytes.Count - search.Count; ++i) {
+        if (bytes[i] != search[0]) continue;
+        if (bytes.Count > 1) {
+          var flag = true;
+          for (var j = 1; j < search.Count; ++j) {
+            if (bytes[i + j] == search[j]) continue;
+            flag = false;
+            break;
+          }
+
+          if (!flag) continue;
+          num = i;
+          break;
+        }
+
+        num = i;
+        break;
+      }
+
+      return num;
+    }
+
+    // Hex string to array of bytes
+    private static byte[] StringToByteArray (string hex) {
+      return Enumerable.Range(0, hex.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(hex.Substring(x, 2), 16)).ToArray();
+    }
+
+    private enum Arch {
+      x86 = 0,
+      x64 = 1
+    }
+
+    private enum Version {
+      lowerThan2018_2 = 0,
+      higherThan2018_2 = 1
+    }
+
+    private enum SkinType {
+      Dark = 0,
+      Light = 1
+    }
+  }
 }
--- readme.md
+++ readme.md
@@ -1,9 +1,46 @@
-### Using
-1. Compile and copy UnityDarkSkin.exe to folder with Unity.exe
-2. Run UnityDarkSkin.exe as administrator
+# UnityDarkSkin
 
-* Note: tested on 5.3, 5.4, 2017.2, 2018.2
-* Should not work on 2018.3
+## About
+
+THIS PROGRAM IS NOT CURRENTLY WORKING
+
+Use this command line tool to patch unity.exe for activating its dark theme.
+
+Changes can be reverted by launching the program twice
+The unity.exe file will be backed up into unity.exe.BAK on first launch.
+
+** This is not a CRACK **
+** All it does is change your theme for accessibility purposes. You do not gain any other premium features. **
+
+Based on [UnityDarkSkin from Glushenko](https://github.com/Gluschenko/UnityDarkSkin)
+
+Basically, the program just replaces some hex values in the unity.exe file.
+
+- for the 2018.3 version and more on an x64 system:
+  84C0750833C04883C4305BC38B034883C4305BC3 hex values are replaced by :
+  84C0740833C04883C4305BC38B034883C4305BC3﻿
+
+- for the 2018.3 version and more on an x86 system:
+ **TODO**
+
+- for the 2018.2 version and less on an x64 system:
+ **TODO**
+
+- for the 2018.2 version and less on an x86 system:
+ **TODO**
+
+## Usage
+
+1. Compile. The executable is located into the bin\Debug (or bin\Release) sub-folder. Build has be tested using Jetbrain Rider IDE, but should work with visual studio.
+2. (optional) copy UnityDarkSkin.exe to folder with Unity.exe
+3. (optional) drag unity.exe file on the UnityDarkSkin.exe executable (in that case, the executable must have administrator rights)
+4. OR (optional) run UnityDarkSkin.exe as administrator.
+5. Answer to program's questions and (optional) Give the unity.exe file path when asked by the program. Current folder will be used by default if the path is not given.
+
+## Notes
+- Now compatible with unity 2018.3 and 2019 (and perhaps higher versions, but untested)
+- tested on 2018.3 (x64), 2019.1 beta (x64)
+- no tested on 5.3, 5.4, 5.6, 2017.2, 2017.4, 2018.2
 
 ![](Media/Preview.jpg)
 ![](Media/LightSkin.jpg)
